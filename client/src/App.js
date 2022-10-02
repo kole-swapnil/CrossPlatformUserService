cbaa516eed85dcce7928b634593dbefc3c05bc61
Srutip04 <subuhishruti75@gmail.com>
2022-10-03 02:08:50 +0530

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
-
-import './App.css';
+import Signup from "./components/signup/signup.jsx";
+import Login from "./components/login/login.jsx";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+ return (
+   <div className="App">
+     <Routes>
+       <Route exact path="/" element={<Signup />} />
+       <Route exact path="/login" element={<Login />} />
+     </Routes>
+   </div>
+ );
 }
 
 export default App;
