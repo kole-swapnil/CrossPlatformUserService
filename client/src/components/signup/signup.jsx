eefbed1698544a31fe777f74c1d59cc9268a028a
Srutip04 <subuhishruti75@gmail.com>
2022-10-11 17:13:55 +0530

    Cover Img upload function

--- client/src/components/signup/signup.jsx
+++ client/src/components/signup/signup.jsx
@@ -154,7 +154,48 @@ const postImgDetails = (pics) =>{
 }
 
 const postCoverImg = (pics) =>{
+  if (pics === undefined) {
+    toast({
+      title: "Please Select an Image!",
+      status: "warning",
+      duration: 5000,
+      isClosable: true,
+      position: "bottom",
+    });
+    return;
+  }
+  console.log(pics);
 
+  if (pics.type === "image/jpeg" || pics.type === "image/png") {
+    const data = new FormData();
+    data.append("file", pics);
+    data.append("upload_preset", "CoverImg");
+    data.append("cloud_name", "crossuser");
+    fetch("https://api.cloudinary.com/v1_1/crossuser/image/upload", {
+      method: "post",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setPic(data.url.toString());
+        console.log(data.url.toString());
+        // setPicLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        // setPicLoading(false);
+      });
+  } else {
+    toast({
+      title: "Please Select an Image!",
+      status: "warning",
+      duration: 5000,
+      isClosable: true,
+      position: "bottom",
+    });
+    // setPicLoading(false);
+    return;
+  }
 }
   return (
     <div>
